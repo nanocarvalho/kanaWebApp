8b1a54d1c2d9372b5364bddfae628e1fe7b0563e
Nano Carvalho <nanoc.contato@gmail.com>
2022-11-23 15:15:29 -0300

    favicon added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Todo:
-- PWA
+# A simple Kana quiz web app
+My goal was to try making something with pwa.
 
-For now, the minimum is done haha.
-[Online](https://kanawebapp.pages.dev)
+For now, the basic is complete, you can see the final [Online](https://kanawebapp.pages.dev)!
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;500;700&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
 		    <meta name="description" content="A simple Kana quiz" />
 		    <meta name="robots" content="all" />
 		    <meta name="author" content="Nano Carvalho" />
